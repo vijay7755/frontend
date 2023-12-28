98ff090c3b11e25365c3223e9475cebad1f81924
vijay7755 <vijaysekar2295@gmail.com>
2023-12-28 12:12:15 +0530

    dockerized

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node:18-alpine as builder
+
+USER node
+
+RUN mkdir -p /home/node/app
+WORKDIR /home/node/app
+
+COPY --chown=node:node ./package.json ./`
+RUN npm install
+
+COPY --chown=node:node ./ ./`
+
+RUN npm run build
+
+
+
+FROM nginx
+COPY --from=builder --chown=node:node /home/node/app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Docker :D
         </p>
+        <h5>Generated via docker-compose</h5>
         <a
           className="App-link"
           href="https://reactjs.org"
